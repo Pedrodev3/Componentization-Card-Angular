bb3045b6b5a5af4fdb8a05c3295e4b12007ccbf4
Pedro Victor Saraiva de Sa - PrestServ <pedro.sa.prestserv@petrobras.com.br>
2023-03-07 17:45:09 -0300

    :tada: Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "black": "#151515",
+      },
+    },
+  },
+  plugins: [],
+};
